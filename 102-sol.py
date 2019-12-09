9f7b19f497113539c19f45e66b67d3ee0b8fe828
Calvin Zhou <vbigyi@gmail.com>
2019-12-09 02:00:14 -0500

    clean

--- 102-sol.py
+++ 102-sol.py
@@ -2,8 +2,6 @@ import numpy as np
 from datetime import datetime
 startTime = datetime.now()
 
-f = open("p102_triangles.txt")
-
 results = 0
 
 def total_area(ts):
@@ -12,16 +10,17 @@ def total_area(ts):
 def splited_area(s):
 	return np.abs(s[0][0] * s[1][1] - s[0][1] * s[1][0])
 
-for line in f:
-	x1,y1,x2,y2,x3,y3 = [int(i) for i in line.rstrip("\n").split(",")]
-	ts = [[x1, y1, 1], [x2, y2, 1], [x3, y3, 1]]
-	s1 = [[x1, y1], [x2, y2]]
-	s2 = [[x1, y1], [x3, y3]]
-	s3 = [[x2, y2], [x3, y3]]
-	ta = total_area(ts)
-	sa = splited_area(s1) + splited_area(s2) + splited_area(s3)
-	if ta == sa:
-		results += 1
+with open("p102_triangles.txt") as f:
+	for line in f:
+		x1,y1,x2,y2,x3,y3 = [int(i) for i in line.rstrip("\n").split(",")]
+		ts = [[x1, y1, 1], [x2, y2, 1], [x3, y3, 1]]
+		s1 = [[x1, y1], [x2, y2]]
+		s2 = [[x1, y1], [x3, y3]]
+		s3 = [[x2, y2], [x3, y3]]
+		ta = total_area(ts)
+		sa = splited_area(s1) + splited_area(s2) + splited_area(s3)
+		if ta == sa:
+			results += 1
 
 print(results)
 print(datetime.now() - startTime)
