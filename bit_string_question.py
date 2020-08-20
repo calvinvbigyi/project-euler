e9dc7a9fddb2382a725bb42ebf60cca3c8f29b2d
Calvin Zhou <vbigyi@gmail.com>
2020-08-20 15:02:04 -0400

    update

--- bit_string_question.py
+++ bit_string_question.py
@@ -36,13 +36,3 @@ def replace_erin(string):
     return [string.replace('?','{}').format(*p) for p in itertools.product([0,1], repeat=string.count('?'))]
 
 print(replace_erin('?01?'))
-# 1. start from simple solution and want to expand it to the general case in the question
-# 2. successfully implmented part of the question "?" -> "1"
-# 3. he had questions/issues with expanding the solution with two "?"s and he seems that he 
-# could debug his own codes.
-# 4. guiding questions: how to go back to the original list
-# 5. he's thinking about while loop or for loop, but never occurred to him that he could use
-# recursion
-# 6. he wants to go back to the original str_li.
-# 7. he's stuck on the 
-# Loop through before doing any join. 
